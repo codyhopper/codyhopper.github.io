403472a6155fa088beac7ca7d9ae00cd62c03aa5
Cody Hopper <hello@codyhopper.com>
2021-02-11 11:04:09 -0500

    link to js, fix touch links

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+let bg = document.querySelector("body");
+bg.addEventListener('mousemove', function(event) {
+  let x = event.clientX;
+  let y = event.clientY;
+  bg.style.backgroundColor = 'rgba(' + y + ', calc(255 - ((' + x + ') - (' + y + '))), ' + x + ', .05)';
+});
